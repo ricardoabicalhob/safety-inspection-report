538688bc4a2b483e4c13154af59878842a1cedd4
ricardoabicalhob <rbicalho2014@gmail.com>
2024-11-22 22:18:01 -0300

    testando visualização de pdf direto no navegador

---
+++ src/app/api/generate-pdf-2/page.tsx
@@ -0,0 +1,36 @@
+'use server'
+
+import { NextApiRequest, NextApiResponse } from "next";
+import puppeteer from "puppeteer";
+
+export default async function GeneratePdf(req :NextApiRequest, res :NextApiResponse) {
+        function uint8ArrayToBase64(array :Uint8Array) {
+            return btoa(String.fromCharCode.apply(null, array as any));
+        }
+
+        const browser = await puppeteer.launch()
+        const page = await browser.newPage()
+
+        await page.goto('http://ricardoabicalhob.github.io/safety-inspection-report/inscritos')
+        
+        const pdfBuffer = await page.pdf({
+            format: 'A4',
+            printBackground: true,
+            margin: {top: '1cm', right: '1cm', bottom: '1cm', left: '1cm'}
+        })
+
+        const base64Buffer = uint8ArrayToBase64(pdfBuffer)
+        const dataUrl = `data:application/pdf;base64,${base64Buffer}`       
+        
+        await browser.close()
+
+        res?.setHeader('Content-Type', 'application/pdf')
+        //res?.setHeader('Content-Disposition', 'attachment; filename="meu-pdf.pdf"')
+        res?.send(base64Buffer)
+
+        return(
+            <div className="h-screen w-screen">
+                <iframe src={dataUrl} width="100%" height="100%"></iframe>
+            </div>
+        )
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,8 @@
 'use client'
 
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+
 
 
 export default function Home() {
@@ -7,12 +10,14 @@ export default function Home() {
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
         <div>
-          {/* <Button onClick={()=> window.location.href = '/api/generate-pdf'}>Emitir relatório</Button> */}
-          <a href="ricardoabicalhob.github.io/safety-inspection-report/api/generate-pdf" download={'ricardoabicalhob.github.io/safety-inspection-report/api/generate-pdf'}>Baixar PDF</a>
+          <Button onClick={()=> window.location.href = '/api/generate-pdf-2'}>Emitir relatório</Button>
+          {/* <a href="ricardoabicalhob.github.io/safety-inspection-report/api/generate-pdf" download={'ricardoabicalhob.github.io/safety-inspection-report/api/generate-pdf'}>Baixar PDF</a> */}
         </div>
       </main>
       <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
- 
+        <Link href={'/api/generate-pdf-2'}>
+          Baixar o pdf
+        </Link>
       </footer>
     </div>
   );
