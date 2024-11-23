17a6e80850d877c98dd7cd17ed8512c250354724
ricardoabicalhob <rbicalho2014@gmail.com>
2024-11-22 22:50:00 -0300

    ..............

--- src/app/api/generate-pdf/page.tsx
+++ src/app/api/generate-pdf/page.tsx
@@ -4,23 +4,28 @@ import path from 'path'
 import { redirect } from 'next/navigation'
 
 export default async function GeneratePdf() {
-    const browser = await puppeteer.launch()
-    const page = await browser.newPage()
+    // const browser = await puppeteer.launch()
+    // const page = await browser.newPage()
 
-    await page.goto('http://ricardoabicalhob.github.io/safety-inspection-report/inscritos')
+    // await page.goto('http://ricardoabicalhob.github.io/safety-inspection-report/inscritos')
 
-    const downloadsPath = os.homedir() + '/Downloads'
+    // const downloadsPath = os.homedir() + '/Downloads'
 
-    const pdfPath = path.join(downloadsPath, 'meu-pdf.pdf')
+    // const pdfPath = path.join(downloadsPath, 'meu-pdf.pdf')
 
-    await page.pdf({
-        path: pdfPath,
-        format: 'A4',
-        printBackground: true,
-        margin: {top: '1cm', right: '1cm', bottom: '1cm', left: '1cm'}
-    })
+    // await page.pdf({
+    //     path: pdfPath,
+    //     format: 'A4',
+    //     printBackground: true,
+    //     margin: {top: '1cm', right: '1cm', bottom: '1cm', left: '1cm'}
+    // })
 
-    await browser.close()
+    // await browser.close()
     
-    redirect('/')
+    // redirect('/')
+    return(
+        <div>
+            
+        </div>
+    )
 }
